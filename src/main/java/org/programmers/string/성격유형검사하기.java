31310a58bde3e359c4a76b8e7f2c0405d41e27b2
hil679 <hil679922@gmail.com>
2026-02-12 10:53:40 +0900

    [string] retrial

--- src/main/java/org/programmers/string/성격유형검사하기.java
+++ src/main/java/org/programmers/string/성격유형검사하기.java
@@ -1,7 +1,32 @@
 package org.programmers.string;
 import java.util.*;
-public class 성격유형검사하기 {
+
+class 성격유형검사하기 {
     public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < survey.length; i++) {
+
+            if (choices[i] > 4) {
+                hm.put(survey[i].substring(1), hm.getOrDefault(survey[i].substring(1), 0) + choices[i] - 4);
+            } else {
+                hm.put(survey[i].substring(0,1), hm.getOrDefault(survey[i].substring(0,1), 0)+ 4 - choices[i]);
+            }
+        }
+
+        if (hm.getOrDefault("R", 0) >= hm.getOrDefault("T", 0)) answer += "R";
+        else answer += "T";
+        if (hm.getOrDefault("C", 0) >= hm.getOrDefault("F", 0)) answer += "C";
+        else answer += "F";
+        if (hm.getOrDefault("J", 0) >= hm.getOrDefault("M", 0)) answer += "J";
+        else answer += "M";
+        if (hm.getOrDefault("A", 0) >= hm.getOrDefault("N", 0)) answer += "A";
+        else answer += "N";
+        return answer;
+    }
+    
+    public String solution2(String[] survey, int[] choices) {
         String answer = "";
         int i = 0;
         HashMap<String, Integer> report = new HashMap<>();
